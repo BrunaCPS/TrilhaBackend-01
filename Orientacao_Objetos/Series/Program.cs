9b2f1e1eec5b0f578d70cdfcedde4a9f7b2b6799
bruna <bruna.cps003@gmail.com>
2024-02-06 15:29:30 -0300

    outra aplicacao para praticar POO

---
+++ Orientacao_Objetos/Series/Program.cs
@@ -0,0 +1,12 @@
+﻿Episodio ep1 = new Episodio(45,1,"Técnicas de facilitação");
+ep1.AdicionarConvidados("Maria");
+ep1.AdicionarConvidados("Bruna");
+
+Episodio ep2 = new Episodio(50,2,"Técnicas de aprendizado");
+ep2.AdicionarConvidados("John");
+ep2.AdicionarConvidados("Isabel");
+
+Podcast podcast = new Podcast("Podcast dev", "Bruna");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.ExibirDetalhes();
