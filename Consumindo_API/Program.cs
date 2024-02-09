8aaddbca9a04f37dbb74d2a09378855a6d645afb
bruna <bruna.cps003@gmail.com>
2024-02-09 12:22:14 -0300

    feat: criacao da classe MusicasPreferidas e seus metodos

--- Consumindo_API/Program.cs
+++ Consumindo_API/Program.cs
@@ -5,7 +5,7 @@ using (HttpClient client = new HttpClient())
    try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");//não consegue converter do tipo task em string, por isso foi usado o await, para esperar a tarefa ser concluida e em seguida pegar o resultado da tarefa e colocar na variável
-        Console.WriteLine(resposta);
+        //Console.WriteLine(resposta);
 
         //Criar uma lista do JSON baseado no Objeto
         //Desserialização -> Pega o JSON e converte em um objeto manipulável em C#
@@ -13,7 +13,27 @@ using (HttpClient client = new HttpClient())
         //LinqFilter.FiltrarTodosGenerosMusicais(musicas);
         //LinqOrder.ExibirListaArtistasOrdenados(musicas);
         //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "rock");
-        LinqFilter.FiltrarMusicasDeUmArtista(musicas,"Michel Teló");
+        //LinqFilter.FiltrarMusicasDeUmArtista(musicas,"Michel Teló");
+
+        var musicasPreferidasBruna = new MusicasPreferidas("Bruna");
+        musicasPreferidasBruna.AdicionarMusicasFavoritas(musicas[1]);
+        musicasPreferidasBruna.AdicionarMusicasFavoritas(musicas[377]);
+        musicasPreferidasBruna.AdicionarMusicasFavoritas(musicas[4]);
+        musicasPreferidasBruna.AdicionarMusicasFavoritas(musicas[6]);
+        musicasPreferidasBruna.AdicionarMusicasFavoritas(musicas[1467]);
+
+        musicasPreferidasBruna.ExibirMusicasFavoritas();
+
+        var musicasPreferidasBel = new MusicasPreferidas("Bel");
+        musicasPreferidasBel.AdicionarMusicasFavoritas(musicas[500]);
+        musicasPreferidasBel.AdicionarMusicasFavoritas(musicas[637]);
+        musicasPreferidasBel.AdicionarMusicasFavoritas(musicas[428]);
+        musicasPreferidasBel.AdicionarMusicasFavoritas(musicas[13]);
+        musicasPreferidasBel.AdicionarMusicasFavoritas(musicas[71]);
+
+        musicasPreferidasBel.ExibirMusicasFavoritas();
+
+
     }
     catch (Exception ex)
     {
